1b9afa13283916578acef3d599e0fb946b217c15
Drew <dandadrew02@gmail.com>
2022-09-17 10:15:59 -0400

    Add postive and negitive words

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 app = Flask(__name__)
 
+positiveWords = []
+negativeWords = []
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
@@ -28,12 +30,20 @@ class Job:
             'location' : self.location,
             'apply' : self.apply
         }
-@app.route('/jobs')
+@app.route('/jobs', methods = ["GET", "POST"])
 def getJobData():
+    print("hel")
+    if request.method == "POST":
+        positiveWords = list(request.form.to_dict(flat=False).values())[0][0].split(',')
+        negativeWords = list(request.form.to_dict(flat=False).values())[1][0].split(',')
+        print(positiveWords)
+        print(negativeWords)
+       # print(request)
     return render_template("JobTable.html")
 
 @app.route('/api/data/jobs')
 def jobData():
+
     descs = [Job() for x in range(10)]
     # response
     ret = {
@@ -47,21 +57,6 @@ def jobData():
 
 def rankdata():
     pass
-@app.route('/Products', methods=["GET", "POST"])
-def displayProducts():
-    return render_template('ProductsTable.html', title='Products Table', login=getEmail())
-
-@app.route('/api/data/Products')
-def Productdata():
-
-    # response
-    return {
-        'data': [product.to_dict() for product in query],
-        'recordsFiltered': 0,
-        'recordsTotal': 0,
-        'draw': 0,
-    }
-
 
 if __name__ == '__main__':
     app.run()
